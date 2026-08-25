d1bfa56ac5797c84665c9c2de723f1c2963e19ef
Harsh Wardhan <harshwardhan847@gmail.com>
2026-08-25 09:30:33 +0530

    phase 1: added prisma schema and basic project setup

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,6 +1,9 @@
-import "dotenv/config";
+import { config } from "dotenv";
+import path from "node:path";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+
+config({ path: path.join(import.meta.dir, ".env") });
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
